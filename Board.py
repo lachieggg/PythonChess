6d088083a477e96fb631352e2f84c0df23e77c48
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-08-25 11:30:18 +1000

    Cleaned up Board class and added board image loading functionality

--- Board.py
+++ Board.py
@@ -34,28 +34,24 @@ class Board:
 
     def setup_pieces(self):
         self.pieces = []
-        # kings
+        # white
         self.pieces.append(Piece('WK', 1, 'E', 1))
-        self.pieces.append(Piece('BK', 0, 'E', 8))
-        # queens
         self.pieces.append(Piece('WQ', 1, 'D', 1))
-        self.pieces.append(Piece('BQ', 0, 'D', 8))
-        # rooks
         self.pieces.append(Piece('WR', 1, 'A', 1))
         self.pieces.append(Piece('WR', 1, 'H', 1))
-        self.pieces.append(Piece('BR', 0, 'A', 8))
-        self.pieces.append(Piece('BR', 0, 'H', 8))
-        # knights
         self.pieces.append(Piece('WH', 1, 'B', 1))
         self.pieces.append(Piece('WH', 1, 'G', 1))
-        self.pieces.append(Piece('BH', 0, 'B', 8))
-        self.pieces.append(Piece('BH', 0, 'G', 8))
-        # bishops
         self.pieces.append(Piece('WB', 1, 'C', 1))
         self.pieces.append(Piece('WB', 1, 'F', 1))
+        for char in self.chars: self.pieces.append(Piece('WP', 1, char, 1))
+
+        # black
+        self.pieces.append(Piece('BK', 0, 'E', 8))
+        self.pieces.append(Piece('BQ', 0, 'D', 8))
+        self.pieces.append(Piece('BR', 0, 'A', 8))
+        self.pieces.append(Piece('BR', 0, 'H', 8))
+        self.pieces.append(Piece('BH', 0, 'B', 8))
+        self.pieces.append(Piece('BH', 0, 'G', 8))
         self.pieces.append(Piece('BB', 0, 'C', 8))
         self.pieces.append(Piece('BB', 0, 'F', 8))
-        # pawns
-        for char in self.chars:
-            self.pieces.append(Piece('WP', 1, char, 1))
-            self.pieces.append(Piece('BP', 0, char, 7))
+        for char in self.chars: self.pieces.append(Piece('BP', 0, char, 7))
--- Window.py
+++ Window.py
@@ -13,13 +13,14 @@ from pygame.locals import (
 )
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 800
 
 class Window:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.screen.fill((255,255,255))
+        self.screen.fill((0, 0, 0))
+        self.board_img = pygame.image.load_extended(os.getcwd() + '/assets/board.png')
 
     def main(self):
         running = True
@@ -33,11 +34,11 @@ class Window:
                 elif event.type == QUIT:
                     running = False
 
-            self.screen.fill((255, 253, 195))
-            pygame.draw.circle(self.screen, (255, 255, 255), (250, 250), 75)
+            #self.screen.fill((255, 253, 195))
+            #pygame.draw.circle(self.screen, (255, 255, 255), (250, 250), 75)
 
             pygame.display.flip()
-            print(os.getcwd() + '/assets/WhiteKing.png')
-            pygame.image.load_extended(os.getcwd() + '/assets/WhiteKing.png')
+            #print(os.getcwd() + '/assets/board.png')
+            self.screen.blit(self.board_img, (50, 50))
 
         pygame.quit()
