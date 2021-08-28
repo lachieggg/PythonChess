f8e57f0a4338152884cc53232088028ec0550b00
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-08-28 10:59:22 +1000

    Added new assets, modified Piece classes

--- pieces/Bishop.py
+++ pieces/Bishop.py
@@ -2,4 +2,6 @@ from pieces.Piece import Piece
 
 class Bishop(Piece):
     def __init__(self, type, colour, char, num):
-        super().__init__(type, colour, char, num)
+        filename = 'WhiteBishop.png' if colour == 'W' else 'BlackBishop.png'
+
+        super().__init__(type, colour, char, num, filename)
--- pieces/Piece.py
+++ pieces/Piece.py
@@ -1,12 +1,11 @@
 
 class Piece:
-    def __init__(self, type, colour, char, num):
+    def __init__(self, type, colour, char, num, filename):
         self.type = type        # eg. K for King, Q for Queen
         self.char = char        # horizontal position
         self.num = num          # vertical position
         self.colour = colour    # 'W' or 'B'
+        self.filename = filename
 
     def __repr__(self):
-        str = self.colour + ' '
-        str += self.type + ' ' + repr(self.char) + ' ' + repr(self.num) + '\n'
-        return str
+        return str(self.__dict__())
